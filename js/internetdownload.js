dbf5c5216e136cce69e5b478a695b5ffdc571d62
inaam husain <inaam@reach1to1.com>
2015-10-04 11:17:20 +0530

    Adding IE version changes
    Adding IE version changes for specific

--- js/internetdownload.js
+++ js/internetdownload.js
@@ -53,7 +53,7 @@ function JSONToCSVConvertor(JSONData,ShowLabel) {
 function msieversion() {
 	var ua = window.navigator.userAgent; 
 	var msie = ua.indexOf("MSIE "); 
-	if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) // If Internet Explorer, return version number 
+	if (msie != -1 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) // If Internet Explorer, return version number 
 	{
 		return true;
 	} else { // If another browser, 
